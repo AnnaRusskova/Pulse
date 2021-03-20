977ffd09e36528700f768f4b14cef635f25c9b00
Anna <anna.russkova@gmail.com>
2021-03-20 17:58:34 +0200

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
@@ -50,4 +50,82 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //Modal
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('fast');
+    });
+    // $('.button_mini').on('click', function() {
+    //     $('.overlay, #order').fadeIn('slow');
+    // });
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    // $('.feed-form').validate();
+    // $('#consultation-form form').validate();
+    // $('#consultation form').validate({
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             minlength: 2
+    //           },
+    //         phone: "required",
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     messages: {
+    //             name: {
+    //               required: "Пожалуйста, введите свое имя",
+    //               minlength: jQuery.validator.format("Введите минимум {0} символа!")
+    //         },
+    //         phone: "Пожалуйста, введите свой номер телефона",
+    //         email: {
+    //           required: "Пожалуйста, введите свою почту",
+    //           email: "Неправильно введен адрес почты"
+    //         }
+    //       }
+    // });
+    // $('#order form').validate();
+    
+    function validateForms (form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                  },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                    name: {
+                      required: "Пожалуйста, введите свое имя",
+                      minlength: jQuery.validator.format("Введите минимум {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+              }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+38 (999) 999-99-99");
 });
